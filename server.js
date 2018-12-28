6c0725ae99ccf1eb36409bb14e32992ff8b8f9e3
Willem Essenstam <wessenstam@gmail.com>
2018-12-28 18:25:51 +0100

    Ready to be pushed onto the Raspberry Pi

--- server.js
+++ server.js
@@ -4,49 +4,21 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
+const morgan=require('morgan')
 
-// Configure MySQL connection
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'nutanix/4u',
-	database: 'node_project'
-  })
 
-//Establish MySQL connection
-connection.connect(function(err) {
-   if (err) 
-      throw err
-   else {
-       console.log('Connected to MySQL');
-       // Start the app when connection is ready
-       app.listen(3000);
-       console.log('Server listening on port 3000');
- }
-});
+app.use(morgan('short'))
+app.listen(3000);
+console.log('Server listening on port 3000');
 
 app.use(bodyParser.json())
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+ '/myfile.html'));
-});
+
 
 app.post('/', function(req, res) {
 
-    var jsondata = req.body;
-    var values = [];
-
-    for(var i=0; i< jsondata.length; i++)
-        values.push([jsondata[i].name,jsondata[i].age]);
-
-    //Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
-    connection.query('INSERT INTO members (name, age) VALUES ?', [values], function(err,result) {
-        if(err) {
-            res.send('Error');
-        }
-        else {
-            res.send('Success');
-        }
-    });
+    var jsondata = req.body
+    console.log('====== Object.keys ======')
+    console.log(jsondata)
+    res.end()
 });
